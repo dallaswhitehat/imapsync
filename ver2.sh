59928c915a9f7a43429d6b21b358694126f6e3cf
sakoshi <teen9xpro@gmail.com>
2015-07-27 13:45:28 +0700

    update option
    --regexflag 's/\$\s//g' \
    --noauthmd5 \
    --split1 100 \
    --split2 100

--- ver2.sh
+++ ver2.sh
@@ -2,6 +2,8 @@
 SERVER1=imap.mail.yahoo.com
 SERVER2=pod51011.outlook.com
 ADMINUSER='sysadmin@C1707.onmicrosoft.com'
+PF1=/tmp/pass1
+PF2=/tmp/pass2
 
 echo Looping on account credentials found in file.txt
 echo
@@ -10,30 +12,37 @@ echo
     do 
         { echo "$u1" | egrep "^#" ; } > /dev/null && continue # this skip commented lines in file.txt
         echo "==== Moving password $u1 to passfile ===="
-		echo "$p1" > ~/pass1
-		chmod 600 ~/pass1
-		echo "$p2" > ~/pass2
-		chmod 600 ~/pass2
+		echo "$p1" > ${PF1}
+		chmod 600 ${PF1}
+		echo "$p2" > ${PF2}
+		chmod 600 ${PF2}
 		echo "==== Syncing user $u1 to user $u2 ===="
 imapsync \
 --host1 ${SERVER1} \
 --user1 "$u1" \
---passfile1 ~/pass1 \
+--passfile1 ${PF1} \
 --authmech1 PLAIN \
 --ssl1 \
 --host2 ${SERVER2} \
 --authuser2 ${ADMINUSER} \
 --user2 "$u2" \
---passfile2 ~/pass2 \
+--passfile2 ${PF2} \
 --ssl2 \
 --nocheckmessageexists \
 --syncinternaldates \
 --usecache \
 --useheader ALL \
---maxsize 10000000 \
---maxlinelength 9900 \
---regexflag 's/\\Flagged//g' \
---disarmreadreceipts
+--regexflag 's/.*?(?:(\\(?:Answered|Flagged|Deleted|Seen|Draft)\s?)|$)/defined($1)?$1:q()/eg' \
+--regexflag 's/\$\s//g' \
+--disarmreadreceipts \
+--fast \
+--noreleasecheck \
+--noauthmd5 \
+--split1 100 \
+--split2 100
+#--delete2duplicatesv \#
+#--allowsizemismatch \#
+#--maxlinelength 990000#
 
         echo "==== End syncing user $u1 to user $u2 ===="
 		echo "==== Removing Passfile ===="
